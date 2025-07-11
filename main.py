a436f1b038bd9e05d81a0337d071fb515853f838
Tarek-AL-Saleh <tarek.z.alsaleh@gmail.com>
2025-07-11 21:46:21 +0300

    Task 6

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 
-# Load saved model
 model = joblib.load("news_sentiment_classifier_model.joblib")
+
+
 
 app = FastAPI()
 
-# Fix CORS error when calling from HTML
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all domains
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Define request body format
+
+
 class NewsInput(BaseModel):
     text: str
 
-# Prediction route
+
+
 @app.post("/predict")
 async def predict_sentiment(input: NewsInput):
-    prediction = str(model.predict([input.text])[0] ) # 0 or 1
+    prediction = model.predict([input.text])[0]# 0 or 1
     label = "negative" if prediction == 0 else "positive" 
-    return {"label": prediction}
+    return {"label": label}
